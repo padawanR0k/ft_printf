2537c63471f9769b3f26f381bcf10250936cb8cb
padawanR0k <padawanR0k@gmail.com>
2021-07-27 12:07:42 +0900

    bonus 파일 복구

--- Makefile
+++ Makefile
@@ -11,9 +11,23 @@ SRCS =	\
 	./util/ft_strchr.c \
 	./util/ft_strlen.c \
 
+BONUS_SRCS =	\
+	./bonus/ft_printf_bonus.c \
+	./bonus/process_bonus.c \
+	./bonus/process2_bonus.c \
+	./bonus/util_option_bonus.c \
+	./bonus/util_print_bonus.c \
+	./bonus/util_print_blank_bonus.c \
+	./bonus/util_print_zero_bonus.c \
+	./bonus/util/ft_isdigit_bonus.c \
+	./bonus/util/ft_strchr_bonus.c \
+	./bonus/util/ft_strlen_bonus.c \
+
+
 NAME =	libftprintf.a
 
 OBJS =	$(SRCS:.c=.o)
+BONUS_OBJS =	$(BONUS_SRCS:.c=.o)
 
 all :	$(NAME)
 
@@ -24,12 +38,13 @@ $(NAME):	$(OBJS)
 	ar rcs $(NAME) $(OBJS)
 
 clean :
-	rm -f $(OBJS)
+	rm -f $(OBJS) $(BONUS_OBJS)
 
 fclean :	clean
 	rm -f $(NAME)
 
-bonus :	$(NAME)
+bonus :	$(BONUS_OBJS)
+	ar rcs $(NAME) $(BONUS_OBJS)
 
 re :	fclean all
 
---
+++ bonus/process2_bonus.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process2.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yurlee <yurlee@student.42.kr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/24 12:43:43 by yurlee            #+#    #+#             */
+/*   Updated: 2021/07/26 21:09:37 by yurlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf_bonus.h"
+
+int	process_p(va_list va_ptr, t_word_flags *flags, const char *base)
+{
+	int		len;
+	int		ret;
+	void	*value;
+
+	ret = 0;
+	value = va_arg(va_ptr, void *);
+	flags->value = &value;
+	len = ft_putnbr_len((long long)value, base);
+	if (flags->precision && value == 0)
+		len = 2;
+	else
+		len += 2;
+	if (flags->left_align == OFF)
+		ret += print_blank(flags, len);
+	ret += ft_putstr("0x");
+	if (!(flags->precision && value == 0))
+		ret += ft_putnbr_base((long long)value, base);
+	if (flags->left_align == ON)
+		ret += print_blank(flags, len);
+	return (ret);
+}
+
+int	process_percent(va_list va_ptr, t_word_flags *flags)
+{
+	char			value;
+	int				i;
+	int				ret;
+
+	ret = 0;
+	i = 0;
+	value = va_arg(va_ptr, int);
+	value = '%';
+	flags->value = &value;
+	if (flags->left_align == OFF)
+		ret += print_blank(flags, 1);
+	if (flags->fill_zero == ON)
+		ret += print_zero(flags, 1);
+	ret += ft_putchar(value);
+	if (flags->left_align == ON)
+		ret += print_blank(flags, 1);
+	return (ret);
+}
